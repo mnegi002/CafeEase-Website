dfc2130d4c07c9ecc71b4d9684c908a0ac9af52c
Avi Sharma <avisharmaid@gmail.com>
2023-12-25 22:21:14 +0530

    nabarsticky

--- src/menu/Menu.jsx
+++ src/menu/Menu.jsx
@@ -4,6 +4,7 @@ import classes from './Menu.module.css'
 // import MenuCard from "./Menucard"
 import ItemcardData from './menuItem/Itemcarddata'
 import ItemCard from './menuItem/Itemcard'
+import Carousel from './menuItem/Carousel'
 // import Cart from '../cart/Cart'
 // import { useContext, useState } from 'react'
 // import CartContext from '../cart/cartContextAPI/CartContext'
@@ -31,6 +32,7 @@ const Menu = (props) => {
                 })}
             </div>
              */}
+    <Carousel></Carousel>
 
     {ItemcardData.map((section, index) => (
       <div key={index}>
