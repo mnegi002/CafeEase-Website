4ea685ac86992d95ff596eb4dbffd013830816ad
Mayank Negi <itsmemayank02@gmail.com>
2024-03-31 23:09:33 +0530

    change

--- src/screens/Signup.jsx
+++ src/screens/Signup.jsx
@@ -113,16 +113,16 @@ export default function Signup() {
         <form onSubmit={handleSubmit}>
           <h1>Create Account</h1>
           <div className="social-icons">
-            <a href="#" className="icon">
+            <a href="https://www.gmail.com" target="_blank" className="icon">
               <i className="fa-brands fa-google-plus-g"></i>
             </a>
-            <a href="#" className="icon">
+            <a href="https://www.facebook.com" target="_blank"  className="icon">
               <i className="fa-brands fa-facebook-f"></i>
             </a>
-            <a href="#" className="icon">
+            <a href="https://www.github.com" target="_blank"  className="icon">
               <i className="fa-brands fa-github"></i>
             </a>
-            <a href="#" className="icon">
+            <a href="https://www.linkedin.com" target="_blank" className="icon">
               <i className="fa-brands fa-linkedin-in"></i>
             </a>
           </div>
