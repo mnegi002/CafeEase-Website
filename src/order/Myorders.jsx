04e121a5db8cf4039a42aa87ef2663923d4892a6
Mayank Negi <itsmemayank02@gmail.com>
2024-06-04 08:04:53 +0530

    changes

--- src/order/Myorders.jsx
+++ src/order/Myorders.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import classes from './OrderCard.module.css';
+import React, { useState, useEffect } from "react";
+import classes from "./OrderCard.module.css";
 
 export default function OrderCard() {
   const [orderData, setOrderData] = useState(null);
@@ -11,9 +11,9 @@ export default function OrderCard() {
     const fetchOrderData = async () => {
       try {
         const response = await fetch(`http://localhost:4000/api/myorderData`, {
-          method: 'POST',
+          method: "POST",
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
           body: JSON.stringify({ email: userEmail }),
         });
@@ -22,10 +22,10 @@ export default function OrderCard() {
           const data = await response.json();
           setOrderData(data.orderData);
         } else {
-          console.error('Error fetching order data:', response.statusText);
+          console.error("Error fetching order data:", response.statusText);
         }
       } catch (error) {
-        console.error('Error fetching order data:', error);
+        console.error("Error fetching order data:", error);
       }
     };
 
@@ -34,22 +34,29 @@ export default function OrderCard() {
 
   return (
     <div className={classes.divmain}>
+      {/* <div className={classes.main}> */}
       {orderData && (
         <div className={classes.orderdiv}>
           <h2>Order Date: {orderData.orderDate}</h2>
-            {orderData.items && orderData.items.map((item, index) => (
-          <div className={classes.container}>
-              <div className={classes.box} key={index}>
-                <h3>Name: {item.name}</h3>
-                <h3>Quantity: {item.quantity}</h3>
-                <h4>₹{item.price}</h4>
+          {orderData.items &&
+            orderData.items.map((item, index) => (
+              <div className={classes.container}>
+                <div className={classes.box} key={index} id="box1">
+                  <h3>Name: {item.name}</h3>
+                  <h3>Quantity: {item.quantity}</h3>
+                  <h4>₹{item.price}</h4>
+                </div>
+                {/* <div className={classes.box2} id="box2">
+                    <img src="chai.jpg" alt="img" />
+                  </div> */}
               </div>
-          </div>
             ))}
-          <div className={classes.line}></div>
+            <div className={classes.line}></div>
         </div>
       )}
-      {!orderData && <h1>Oops !! You haven't placed any order yet</h1>}
+      {!orderData && (
+        <h1>Oops !! You haven't placed any order yet &#128542;</h1>
+      )}
     </div>
   );
 }
