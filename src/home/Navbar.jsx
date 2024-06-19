f8824b37738adb4fd017493f4553c080432c9915
Mayank Negi <itsmemayank02@gmail.com>
2024-06-19 16:04:45 +0530

    last changes

--- src/home/Navbar.jsx
+++ src/home/Navbar.jsx
@@ -61,9 +61,6 @@ const Navbar = () => {
                    // transform : isActive ? "color 1s fade-in-out" : '' ,
                })}>My Orders</NavLink></li>   
                }
-              
-               
-              
            </ul>
        </div>
    </div>
